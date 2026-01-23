805061387e4d31ca0451abf5f10796d51c221df7
Rodney Caneda <molp.burnbright@gmail.com>
2026-01-23 11:35:41 +0800

    initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { environment } from '../environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()), 
+    provideFirebaseApp(() => 
+      initializeApp(environment.firebaseConfig)), 
+    provideFirestore(() => getFirestore()), 
+    provideMessaging(() => getMessaging())
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,81 @@
-import { Component, signal } from '@angular/core';
+import { Component, ChangeDetectorRef, signal, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, doc, docData, updateDoc } from '@angular/fire/firestore';
+import { getToken, onMessage} from "firebase/messaging";
+import { Messaging } from '@angular/fire/messaging';
+import { FormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from "@angular/material/slide-toggle"
+import { environment } from '../environment';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    MatSlideToggleModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('waterflow-webapp');
+
+  firestore: Firestore = inject(Firestore);
+  messaging: Messaging = inject(Messaging);
+
+  device: any;
+  messageCount: number = 0;
+
+  constructor(private cdr: ChangeDetectorRef){
+
+    const docRef = doc(this.firestore, 'devices', 'test_device_1');
+    docData(docRef).subscribe(data => {
+        this.device = data;
+        cdr.detectChanges();
+    });
+
+
+  }
+
+  ngOnInit(): void {
+
+
+    this.requestPermission();
+
+    onMessage(this.messaging, (payload) => {
+      alert(JSON.stringify(payload));
+      // ...
+    });
+  }
+
+
+  onValveToggle(){
+    const docRef = doc(this.firestore, 'devices', 'test_device_1');
+    updateDoc(docRef, {"valve_status": this.device.valve_status});
+  }
+ 
+  requestPermission() {
+    console.log('Requesting permission...');
+    Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+      console.log('Notification permission granted.');
+      getToken(this.messaging, {
+       vapidKey: environment.vapidKey,
+        })
+            .then((currentToken: string) => {
+            if (currentToken) {
+                console.log(currentToken);
+            } else {
+                console.log(
+                'No registration token available. Request permission to generate one.'
+                );
+            }
+            })
+            .catch((err: any) => {
+            console.log(err);
+            });
+        }
+    });
+  }
+
 }
